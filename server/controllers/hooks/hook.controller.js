5c6a1331ff1ef4f223f90d6b6e229602e31dff83
Niall Thurrat <nt222fc@student.lnu.se>
2021-03-20 23:19:07 +0100

    funcs created for push + release hooks

--- server/controllers/hooks/hook.controller.js
+++ server/controllers/hooks/hook.controller.js
@@ -8,22 +8,78 @@
 'use strict'
 
 // import User from '../../models/user.model'
-// import createError from 'http-errors'
+import createError from 'http-errors'
 
 /**
-   * Get user
+   * GitLab webhook router function
    * Handling POST requests to endpoint /api/hooks
    *
    * @param {Object} req
    * @param {Object} res
    * @param {Function} next - Next middleware func
-   * @response success gives 200 OK with JSON body
    *
    */
-const hookController = async (req, res, next) => {
-  const resBody = req.body
-  console.log('Im here')
-  res.status(200).json(resBody)
+const hookController = (req, res, next) => {
+  const header = 'X-Gitlab-Event'
+  const hookType = req.header(header)
+
+  // TODO handle no header
+  // TODO authenticate hook
+
+  switch (hookType) {
+    case 'Push Hook':
+      handlePushHook(req, res, next)
+      break
+
+    case 'Release Hook':
+      handleReleaseHook(req, res, next)
+      break
+
+    default:
+      return next(createError(400,
+        'Hook request error'
+      ))
+  }
 }
+
+/**
+   * GitLab webhook handler for Push Events
+   *
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next - Next middleware func
+   *
+   */
+const handlePushHook = (req, res, next) => {
+  try {
+    const projectId = req.body.project_id
+    const commits = req.body.commits
+
+    // TODO handle if there are more than 20 commits in push
+
+    const resBody = {
+      projectId: projectId,
+      commits: commits
+    }
+    res.status(200).json(resBody) // TODO emit event here - remove response json
+  } catch (error) {
+    next(error)
+  }
+}
+
+/**
+   * GitLab webhook handler for Release Events
+   *
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Function} next - Next middleware func
+   *
+   */
+const handleReleaseHook = (req, res, next) => {
+  res.status(200)
+    .json(req.body) // TODO emit event here - remove response json
+}
+
+// console.log('switch 1 works') console.log(`reqBody: ${reqBody}`)
 
 export default hookController

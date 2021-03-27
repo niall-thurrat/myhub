8d1efdca4c5d1dd87e4a17eacb83f6ff9e2b1e24
Niall Thurrat <nt222fc@student.lnu.se>
2021-03-27 17:17:03 +0100

    socket.io connects to group component

--- client/src/components/groups/group.component.js
+++ client/src/components/groups/group.component.js
@@ -6,114 +6,105 @@
  * https://bezkoder.com/react-crud-web-api/
  */
 
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import GroupsService from '../../services/groups.service'
+import socketIOClient from 'socket.io-client'
 
-export default class Group extends Component {
-  constructor (props) {
-    super(props)
-    this.getGroup = this.getGroup.bind(this)
+const SOCKET_SERVER = 'http://localhost:8080'
 
-    this.state = {
-      currentGroup: {
-        groupId: null,
-        full_name: '',
-        description: ''
-      },
-      groupCommits: null
-    }
+const Group = props => {
+  const initialGroupState = {
+    groupId: null,
+    full_name: '',
+    description: ''
   }
 
-  componentDidMount () {
-    this.getGroup(this.props.match.params.id)
-    this.getCommits(this.props.match.params.id)
-  }
+  const [currentGroup, setCurrentGroup] = useState(initialGroupState)
+  const [commits, setCommits] = useState(undefined)
+  const [response, setResponse] = useState({ id: 'change me' })
 
-  getGroup (groupId) {
+  useEffect(() => {
+    getGroup(props.match.params.id)
+    getCommits(props.match.params.id)
+
+    const socket = socketIOClient(SOCKET_SERVER)
+    socket.on('releaseData', data => {
+      setResponse(data)
+    })
+  }, [props.match.params.id])
+
+  const getGroup = groupId => {
     GroupsService.get(groupId)
       .then(response => {
-        this.setState({
-          currentGroup: response.data
-        })
+        setCurrentGroup(response.data)
       })
       .catch(e => {
         console.log(e)
       })
   }
 
-  getCommits (groupId) {
+  const getCommits = groupId => {
     GroupsService.getCommits(groupId)
       .then(response => {
-        this.setState({
-          groupCommits: response.data
-        })
+        setCommits(response.data)
       })
       .catch(e => {
         console.log(e)
       })
   }
 
-  render () {
-    const currentGroup = this.state.currentGroup
-    const groupCommits = this.state.groupCommits
-
-    return (
+  return (
+    <div>
       <div>
+        <h4>Group Dashboard</h4>
         <div>
-          {currentGroup ? (
-            <div>
-              <h4>Group</h4>
-              <div>
-                <label>
-                  <strong>Full Name:</strong>
-                </label>{' '}
-                {currentGroup.full_name}
-              </div>
-              <div>
-                <label>
-                  <strong>Id:</strong>
-                </label>{' '}
-                {currentGroup.groupId}
-              </div>
-              <div>
-                <label>
-                  <strong>Description:</strong>
-                </label>{' '}
-                {currentGroup.description}
-              </div>
-            </div>
-          ) : (
-            <div>
-              <br />
-              <p>Please click on a Group...</p>
-            </div>
-          )}
+          <label>
+            <strong>Full Name:</strong>
+          </label>{' '}
+          {currentGroup.full_name}
         </div>
         <div>
-          {groupCommits ? (
-            <div>
-              <h4>Commits</h4>
-              <div>
-                <label>
-                  <strong>Project 1 id:</strong>
-                </label>{' '}
-                {groupCommits.data[0].projectId}
-              </div>
-              <div>
-                <label>
-                  <strong>Project 1 commit 1:</strong>
-                </label>{' '}
-                {groupCommits.data[0].commits[0].title}
-              </div>
-            </div>
-          ) : (
-            <div>
-              <br />
-              <p>Error: No commit data found...</p>
-            </div>
-          )}
+          <label>
+            <strong>Id:</strong>
+          </label>{' '}
+          {currentGroup.groupId}
+        </div>
+        <div>
+          <label>
+            <strong>Description:</strong>
+          </label>{' '}
+          {currentGroup.description}
         </div>
       </div>
-    )
-  }
+      <div>
+        <div>
+          <p>Response: {response.id}</p>
+        </div>
+        {commits ? (
+          <div>
+            <h4>Commits</h4>
+            <div>
+              <label>
+                <strong>Project 1 id:</strong>
+              </label>{' '}
+              {commits.data[0].projectId}
+            </div>
+            <div>
+              <label>
+                <strong>Project 1 commit 1:</strong>
+              </label>{' '}
+              {commits.data[0].commits[0].title}
+            </div>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Error: No commit data found...</p>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
+
+export default Group

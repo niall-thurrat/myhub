3fe9ecb35204c022f9688c08c061804016e942c7
Niall Thurrat <nt222fc@student.lnu.se>
2021-04-03 23:22:33 +0200

    model added

---
+++ server/models/notification.model.js
@@ -0,0 +1,47 @@
+/**
+ * notification model
+ * @author Niall Thurrat
+ */
+
+import mongoose from 'mongoose'
+require('mongoose-type-url')
+
+const notificationSchema = mongoose.Schema({
+  gitlabProjectId: {
+    type: Number,
+    required: true
+  },
+  gitlabProjectName: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  gitlabCreatedAt: {
+    type: Date,
+    required: false
+  },
+  gitlabCreatedBy: {
+    type: Date,
+    required: false
+  },
+  gitlabInstanceUrl: {
+    type: mongoose.SchemaTypes.Url,
+    required: true
+  },
+  pushCommitCount: {
+    type: Number,
+    required: false
+  },
+  releaseTag: {
+    type: String,
+    required: false
+  }
+}, { timestamps: true })
+
+const Notification =
+  mongoose.model('Notification', notificationSchema)
+
+export default Notification

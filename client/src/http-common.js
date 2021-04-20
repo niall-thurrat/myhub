c3d23dfb37be89171d005694df7f4eb4d7cc1198
Niall Thurrat <nt222fc@student.lnu.se>
2021-04-20 16:01:14 +0200

    axios interceptor redirects to login on 401

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -8,9 +8,25 @@
 
 import axios from 'axios'
 
-export default axios.create({
+const httpCommon = axios.create({
   baseURL: 'http://localhost:8080/api',
   headers: {
     'Content-type': 'application/json'
   }
 })
+
+// redirect unautorised users to login
+httpCommon.interceptors.response.use(
+  response => {
+    console.log('response ok')
+    return response
+  },
+  error => {
+    console.log('now error')
+    if (error.response.status === 401) {
+      window.location = '/login'
+    }
+    return Promise.reject(error)
+  })
+
+export default httpCommon
